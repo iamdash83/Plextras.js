f57556ec7f2134c7e028e071424a0d886424c16e
iAmDash83 <iamdash83@gmail.com>
2018-03-31 04:20:49 -0400

    Update Plextras.user.js

--- Plextras.user.js
+++ Plextras.user.js
@@ -10,7 +10,7 @@
 
 (function () {
     var script = document.createElement('script');
-    script.src = 'https://raw.githubusercontent.com/iamdash83/Plextras.js/master/Plextras.js';
+    script.src = 'https://andrewiankidd.co.uk/ext/mimefix/?gitfile=Plextras.template&cssBGColor=%230039e6&cssTopbarColor=black&showCustomBackgroundImage=false&autohideSidebar=true&autoHideSidebarMobileOnly=false&biggerThumbnailsOnMobile=true&showArtWorkBackground=true&showEpisodeSpecificArtwork=true&ShowExpandedExtras=true&hideMovieExtras=false&hideCastList=false&autoSignIn=false&hideRelatedMedia=false&customHeader=Custom+Links&customLinks=J0VtYnknOidIdHRwOi8vYXBwLmVtYnkudHYnLCdZb3VUdWJlLkFwcCc6J1lvdVR1YmU6Ly8n';
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
 })();
